f47239a6deb7d930b5752014fc1fa49df012ed66
Matt Michie <mmichie@gmail.com>
2023-07-27 15:49:36 -0700

    Add SystemReporter interface with DiskUsage, Cpu and MemoryUsage

--- steelcut/host.go
+++ steelcut/host.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"os/exec"
 	"os/user"
-
+	"regexp"
+	"strconv"
 	"strings"
 
 	"golang.org/x/crypto/ssh"
@@ -13,6 +14,12 @@ import (
 
 type Update struct {
 	// Fields for the Update struct
+}
+type SystemReporter interface {
+	CPUUsage() (float64, error)
+	MemoryUsage() (float64, error)
+	DiskUsage() (float64, error)
+	RunningProcesses() ([]string, error)
 }
 
 type Host interface {
@@ -22,6 +29,7 @@ type Host interface {
 	AddPackage(pkg string) error
 	RemovePackage(pkg string) error
 	UpgradePackage(pkg string) error
+	SystemReporter
 }
 
 type UnixHost struct {
@@ -67,6 +75,97 @@ func (h LinuxHost) RunCommand(cmd string) (string, error) {
 	return h.UnixHost.RunCommand(cmd)
 }
 
+func (h LinuxHost) CPUUsage() (float64, error) {
+	output, err := h.RunCommand("cat /proc/stat | grep '^cpu '")
+	if err != nil {
+		return 0, err
+	}
+
+	// Parse the output to calculate CPU usage.
+	// The format is: cpu  user nice system idle iowait irq softirq steal guest guest_nice
+	fields := strings.Fields(output)
+	if len(fields) < 10 {
+		return 0, fmt.Errorf("unexpected format of /proc/stat")
+	}
+
+	var total float64
+	for _, field := range fields[1:] { // Skip the "cpu" prefix
+		value, err := strconv.ParseFloat(field, 64)
+		if err != nil {
+			return 0, fmt.Errorf("error parsing /proc/stat: %v", err)
+		}
+		total += value
+	}
+
+	// CPU usage is the proportion of total time not spent idle
+	idle, err := strconv.ParseFloat(fields[4], 64) // "idle" is the 5th field
+	if err != nil {
+		return 0, fmt.Errorf("error parsing /proc/stat: %v", err)
+	}
+	usage := 100 * (1 - idle/total)
+
+	return usage, nil
+}
+
+func (h LinuxHost) MemoryUsage() (float64, error) {
+	output, err := h.RunCommand("cat /proc/meminfo")
+	if err != nil {
+		return 0, err
+	}
+
+	// Parse the output to calculate memory usage.
+	// The relevant lines are MemTotal: and MemAvailable:.
+	lines := strings.Split(output, "\n")
+	var memTotal, memAvailable float64
+	for _, line := range lines {
+		if strings.HasPrefix(line, "MemTotal:") {
+			fields := strings.Fields(line)
+			memTotal, err = strconv.ParseFloat(fields[1], 64)
+			if err != nil {
+				return 0, fmt.Errorf("error parsing MemTotal: %v", err)
+			}
+		} else if strings.HasPrefix(line, "MemAvailable:") {
+			fields := strings.Fields(line)
+			memAvailable, err = strconv.ParseFloat(fields[1], 64)
+			if err != nil {
+				return 0, fmt.Errorf("error parsing MemAvailable: %v", err)
+			}
+		}
+	}
+
+	memUsage := 100 * (1 - memAvailable/memTotal)
+
+	return memUsage, nil
+}
+
+func (h LinuxHost) DiskUsage() (float64, error) {
+	output, err := h.RunCommand("df --output=pcent / | tail -n 1")
+	if err != nil {
+		return 0, err
+	}
+
+	// The output is the percentage of disk space used.
+	output = strings.TrimSpace(output)      // Remove leading/trailing whitespace
+	output = strings.TrimRight(output, "%") // Remove trailing %
+	usage, err := strconv.ParseFloat(output, 64)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing disk usage: %v", err)
+	}
+
+	return usage, nil
+}
+
+func (h LinuxHost) RunningProcesses() ([]string, error) {
+	output, err := h.RunCommand("ps aux")
+	if err != nil {
+		return nil, err
+	}
+
+	// The output is one line per running process.
+	lines := strings.Split(output, "\n")
+	return lines[1:], nil // Skip the header line
+}
+
 func (h MacOSHost) ListPackages() ([]string, error) {
 	return h.PackageManager.ListPackages(h.UnixHost)
 }
@@ -90,6 +189,88 @@ func (h MacOSHost) CheckUpdates() ([]Update, error) {
 
 func (h MacOSHost) RunCommand(cmd string) (string, error) {
 	return h.UnixHost.RunCommand(cmd)
+}
+
+func (h MacOSHost) CPUUsage() (float64, error) {
+	output, err := h.RunCommand("top -l 1 | grep 'CPU usage'")
+	if err != nil {
+		return 0, err
+	}
+
+	// The output should look like: CPU usage: 7.78% user, 12.22% sys, 80.0% idle
+	// Split the output by ',' to get the idle percentage.
+	parts := strings.Split(output, ",")
+	if len(parts) < 3 {
+		return 0, fmt.Errorf("unexpected output from top command: %s", output)
+	}
+
+	// The idle percentage is the third part. Trim the spaces and '%' and parse to float.
+	idleStr := strings.TrimSpace(parts[2])
+	idleStr = strings.TrimSuffix(idleStr, "% idle")
+
+	idle, err := strconv.ParseFloat(idleStr, 64)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing idle percentage: %v", err)
+	}
+
+	// The CPU usage is 100% minus the idle percentage.
+	cpuUsage := 100 - idle
+
+	return cpuUsage, nil
+}
+
+func (h MacOSHost) DiskUsage() (float64, error) {
+	output, err := h.RunCommand("df / | tail -1 | awk '{print $5}'")
+	if err != nil {
+		return 0, err
+	}
+
+	// The output is the percentage of disk space used.
+	// You'll need to remove the trailing % and convert to a float64.
+	output = strings.TrimSuffix(output, "%")
+	usage, err := strconv.ParseFloat(output, 64)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing disk usage: %v", err)
+	}
+
+	return usage, nil
+}
+
+func (h MacOSHost) RunningProcesses() ([]string, error) {
+	output, err := h.RunCommand("ps -axco command")
+	if err != nil {
+		return nil, err
+	}
+
+	// The output is one line per running process.
+	// You can split it into a slice.
+	processes := strings.Split(output, "\n")
+	return processes, nil
+}
+
+func (h MacOSHost) MemoryUsage() (float64, error) {
+	output, err := h.RunCommand("vm_stat | grep 'Pages active'")
+	if err != nil {
+		return 0, err
+	}
+
+	// output looks like "Pages active:                        837161.".
+	// So we need to extract the number.
+	re := regexp.MustCompile("[0-9]+")
+	match := re.FindString(output)
+	if match == "" {
+		return 0, fmt.Errorf("could not parse memory info: %s", output)
+	}
+
+	activeMemory, err := strconv.Atoi(match)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing active memory pages: %v", err)
+	}
+
+	// Each page is 4096 bytes.
+	// The total memory is the number of active pages * the size of each page.
+	totalMemory := float64(activeMemory) * 4096
+	return totalMemory / 1024 / 1024, nil
 }
 
 type HostOption func(*UnixHost)
