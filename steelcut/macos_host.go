36d9a58f79a534f83988703901de7a2e90b45169
Matt Michie <mmichie@gmail.com>
2023-08-14 15:32:24 -0700

    Refactor MacOSHost methods and add comments for clarity
    - Defined constants for memory page size and utilized regex for parsing.
    - Improved clarity and performance of methods like `CPUUsage` and
      `MemoryUsage` by refining the logic and employing regular expressions.
    - Enhanced documentation for MacOSHost methods, providing more insight
      into their functionalities.

--- steelcut/macos_host.go
+++ steelcut/macos_host.go
@@ -7,21 +7,26 @@ import (
 	"strings"
 )
 
+const PageSize = 4096 // size of each memory page in bytes
+
 type MacOSHost struct {
 	*UnixHost
 	PackageManager PackageManager
 }
 
+// Reboot restarts the macOS host.
 func (h MacOSHost) Reboot() error {
 	_, err := h.RunCommand("sudo reboot")
 	return err
 }
 
+// Shutdown turns off the macOS host.
 func (h MacOSHost) Shutdown() error {
 	_, err := h.RunCommand("sudo shutdown -h now")
 	return err
 }
 
+// Pass through the package management operations to the PackageManager.
 func (h MacOSHost) ListPackages() ([]string, error) {
 	return h.PackageManager.ListPackages(h.UnixHost)
 }
@@ -51,42 +56,35 @@ func (h MacOSHost) RunCommand(cmd string) (string, error) {
 	return h.UnixHost.RunCommand(cmd)
 }
 
+// CPUUsage retrieves the CPU usage for the macOS host.
+var idleRegex = regexp.MustCompile(`(\d+\.\d+)% idle`)
+
 func (h MacOSHost) CPUUsage() (float64, error) {
 	output, err := h.RunCommand("top -l 1 | grep 'CPU usage'")
 	if err != nil {
 		return 0, err
 	}
 
-	// The output should look like: CPU usage: 7.78% user, 12.22% sys, 80.0% idle
-	// Split the output by ',' to get the idle percentage.
-	parts := strings.Split(output, ",")
-	if len(parts) < 3 {
-		return 0, fmt.Errorf("unexpected output from top command: %s", output)
+	matches := idleRegex.FindStringSubmatch(output)
+	if len(matches) != 2 {
+		return 0, fmt.Errorf("unexpected CPU usage output: %s", output)
 	}
 
-	// The idle percentage is the third part. Trim the spaces and '%' and parse to float.
-	idleStr := strings.TrimSpace(parts[2])
-	idleStr = strings.TrimSuffix(idleStr, "% idle")
-
-	idle, err := strconv.ParseFloat(idleStr, 64)
+	idle, err := strconv.ParseFloat(matches[1], 64)
 	if err != nil {
 		return 0, fmt.Errorf("error parsing idle percentage: %v", err)
 	}
 
-	// The CPU usage is 100% minus the idle percentage.
-	cpuUsage := 100 - idle
-
-	return cpuUsage, nil
+	return 100 - idle, nil
 }
 
+// DiskUsage retrieves the disk usage for the macOS host.
 func (h MacOSHost) DiskUsage() (float64, error) {
 	output, err := h.RunCommand("df / | tail -1 | awk '{print $5}'")
 	if err != nil {
 		return 0, err
 	}
 
-	// The output is the percentage of disk space used.
-	// You'll need to remove the trailing % and convert to a float64.
 	output = strings.TrimSuffix(output, "%")
 	usage, err := strconv.ParseFloat(output, 64)
 	if err != nil {
@@ -96,17 +94,18 @@ func (h MacOSHost) DiskUsage() (float64, error) {
 	return usage, nil
 }
 
+// RunningProcesses retrieves a list of running processes on the macOS host.
 func (h MacOSHost) RunningProcesses() ([]string, error) {
 	output, err := h.RunCommand("ps -axco command")
 	if err != nil {
 		return nil, err
 	}
 
-	// The output is one line per running process.
-	// You can split it into a slice.
-	processes := strings.Split(output, "\n")
-	return processes, nil
+	return strings.Split(output, "\n"), nil
 }
+
+// MemoryUsage retrieves the active memory usage for the macOS host.
+var activeMemoryRegex = regexp.MustCompile(`Pages active:\s+(\d+)`)
 
 func (h MacOSHost) MemoryUsage() (float64, error) {
 	output, err := h.RunCommand("vm_stat | grep 'Pages active'")
@@ -114,21 +113,15 @@ func (h MacOSHost) MemoryUsage() (float64, error) {
 		return 0, err
 	}
 
-	// output looks like "Pages active:                        837161.".
-	// So we need to extract the number.
-	re := regexp.MustCompile("[0-9]+")
-	match := re.FindString(output)
-	if match == "" {
+	matches := activeMemoryRegex.FindStringSubmatch(output)
+	if len(matches) != 2 {
 		return 0, fmt.Errorf("could not parse memory info: %s", output)
 	}
 
-	activeMemory, err := strconv.Atoi(match)
+	activeMemory, err := strconv.Atoi(matches[1])
 	if err != nil {
 		return 0, fmt.Errorf("error parsing active memory pages: %v", err)
 	}
 
-	// Each page is 4096 bytes.
-	// The total memory is the number of active pages * the size of each page.
-	totalMemory := float64(activeMemory) * 4096
-	return totalMemory / 1024 / 1024, nil
+	return float64(activeMemory * PageSize) / 1024 / 1024, nil
 }
