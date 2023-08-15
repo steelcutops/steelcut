3fae71a112cea49af1a3869243e6a447c384dbab
Matt Michie <mmichie@gmail.com>
2023-08-15 13:39:07 -0700

    Add unit tests for Apt and Brew package managers
    - Introduced tests for listing, adding, removing, upgrading packages
      using the AptPackageManager.
    - Incorporated tests for the same functionalities for
      BrewPackageManager.
    - Covered tests to check for OS updates for both package managers.

--- steelcut/package_manager_test.go
+++ steelcut/package_manager_test.go
@@ -70,3 +70,94 @@ func TestYumPackageManager(t *testing.T) {
 	}, upgrades)
 
 }
+
+func TestAptPackageManager(t *testing.T) {
+	mockExecutor := new(MockCommandExecutor)
+	logger := log.New(os.Stdout, "test: ", log.Lshortfile)
+
+	packageManager := AptPackageManager{
+		Executor: mockExecutor,
+		Logger:   logger,
+	}
+
+	// Test: ListPackages
+	mockExecutor.On("RunCommand", "apt list --installed", false).Return("package1\npackage2\n", nil)
+	packages, err := packageManager.ListPackages(nil)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{"package1", "package2", ""}, packages)
+
+	// Test: AddPackage
+	mockExecutor.On("RunCommand", "apt install -y package3", true).Return("", nil)
+	err = packageManager.AddPackage(nil, "package3")
+	assert.Nil(t, err)
+
+	// Test: RemovePackage
+	mockExecutor.On("RunCommand", "apt remove -y package1", true).Return("", nil)
+	err = packageManager.RemovePackage(nil, "package1")
+	assert.Nil(t, err)
+
+	// Test: UpgradePackage
+	mockExecutor.On("RunCommand", "apt upgrade -y package2", true).Return("", nil)
+	err = packageManager.UpgradePackage(nil, "package2")
+	assert.Nil(t, err)
+
+	// Test: CheckOSUpdates
+	mockExecutor.On("RunCommand", "apt update", true).Return("", nil)
+	mockExecutor.On("RunCommand", "apt list --upgradable", true).Return("package1 update\npackage2 update\n", nil)
+	updates, err := packageManager.CheckOSUpdates(nil)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{"package1 update", "package2 update", ""}, updates)
+
+}
+
+func TestBrewPackageManager(t *testing.T) {
+	mockExecutor := new(MockCommandExecutor)
+	logger := log.New(os.Stdout, "test: ", log.Lshortfile)
+
+	packageManager := BrewPackageManager{
+		Executor: mockExecutor,
+		Logger:   logger,
+	}
+
+	// Test: ListPackages
+	mockExecutor.On("RunCommand", "brew list --version", false).Return("package1 1.0\npackage2 2.0\n", nil)
+	packages, err := packageManager.ListPackages(nil)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{"package1 1.0", "package2 2.0", ""}, packages)
+
+	// Test: AddPackage
+	mockExecutor.On("RunCommand", "brew install package3", false).Return("", nil)
+	err = packageManager.AddPackage(nil, "package3")
+	assert.Nil(t, err)
+
+	// Test: RemovePackage
+	mockExecutor.On("RunCommand", "brew uninstall package1", false).Return("", nil)
+	err = packageManager.RemovePackage(nil, "package1")
+	assert.Nil(t, err)
+
+	// Test: UpgradePackage
+	mockExecutor.On("RunCommand", "brew upgrade package2", false).Return("", nil)
+	err = packageManager.UpgradePackage(nil, "package2")
+	assert.Nil(t, err)
+
+	// Test: CheckOSUpdates
+	mockExecutor.On("RunCommand", "brew outdated", false).Return("package1 2.0\npackage2 3.0\n", nil)
+	updates, err := packageManager.CheckOSUpdates(nil)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{"package1 2.0", "package2 3.0", ""}, updates)
+
+	// Test: UpgradeAll
+	mockExecutor.On("RunCommand", "brew upgrade", false).Return("package1 2.0\npackage2 3.0\n", nil)
+	upgrades, err := packageManager.UpgradeAll(nil)
+	assert.Nil(t, err)
+	assert.Equal(t, []Update{
+		{
+			PackageName: "package1",
+			Version:     "2.0",
+		},
+		{
+			PackageName: "package2",
+			Version:     "3.0",
+		},
+	}, upgrades)
+}
